b9017be8d6c5a27e84948714080b0a1f0d92ef9a
gutingyue <1335282635@qq.com>
2020-04-24 15:30:53 +0800

    类的声明与定义 class大小的计算

--- C++ test05/C++ tast05/test.cpp
+++ C++ test05/C++ tast05/test.cpp
@@ -1,17 +1,42 @@
-#include <iostream>
-using namespace std;
+#include "Person.h"
 
+/*
 void ForTest(){
 	int arr[] = { 1, 2, 3, 4, 5, 6 };
-	//范围for
+	范围for
 	for (auto& a : arr){
 		a++;
 		cout << a << " ";
 	}
-}
+} 
+*/
 
+struct Student{ //学生类
+	//学生对象初始化
+	void SetStudent(char* name,int age){
+		strcpy(_name, name);
+		_age = age;
+	}
 
+	void StudentPrint(){
+		cout << "my name is " << _name << endl;
+		cout << "I am " << _age << " years old." << endl;
+	}
+
+	char _name[10];
+	int _age;
+};
 int main(){
-	ForTest();
+	Student s1;
+	s1.SetStudent("Toin", 21);
+	s1._age = 22;
+	s1.StudentPrint();
+
+	Person p1;
+	p1.SetPerson("Bear", "man", 16);
+	p1.SetPerson("Toin", "man", 20);
+
+	int Size_p1=sizeof(p1);
+	cout << "size of p1 " << Size_p1 << endl;//10+10+4 = 24
 	return 0;
 }
