29883ca76e9cd3e2b825b7df2ec457d438e1bd88
gutingyue <1335282635@qq.com>
2020-05-06 22:06:07 +0800

    C++11中的类成员初始化

---
+++ C++ test09/C++ test09/test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+
+class B
+{
+public:
+	B(int b){
+		_b = b;
+	}
+
+	int _b;
+};
+
+int Print(int num){
+	cout << "接收缺省值" << endl;
+	return num;
+}
+
+//C++11 初始化（定义时赋初值）
+class A
+{
+public:
+	A(int a)
+		:_a(a)
+		,c(10)
+	{
+		a = 30;
+	}
+
+private:
+	int _a = Print(10);
+	int* b = (int*)malloc(sizeof(int));
+	B c;
+};
+
+int main(){
+	A test(20);
+
+	return 0;
+}
