b530f40896ccb0845a745089b37a5a5f1d0a2c24
gutingyue <1335282635@qq.com>
2020-08-09 22:20:40 +0800

    base64 解码逻辑实现及验证

--- C++项目/base64/base64/Base64.cpp
+++ C++项目/base64/base64/Base64.cpp
@@ -3,6 +3,7 @@
 //编码
 string Base64::Encode(const string& strData){
 	string strEncode;
+
 	//编码表
 	string EncodeTable("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
 	unsigned char temp[3];
@@ -57,6 +58,58 @@ string Base64::Encode(const string& strData){
 //解码
 string Base64::Decode(const string& strData){
 	string strDecode;
+
+	//解码表
+	const char DecodeTable[] =
+	{
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		62, // '+'
+		0, 0, 0,
+		63, // '/'
+		52, 53, 54, 55, 56, 57, 58, 59, 60, 61, // '0'-'9'
+		0, 0, 0, 0, 0, 0, 0,
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+		13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, // 'A'-'Z'
+		0, 0, 0, 0, 0, 0,
+		26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+		39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, // 'a'-'z'
+	};
+
+	size_t value;
+	for(int i = 0; i < strData.size();){
+		if (strData[i] != '\r'&&strData[i + 1] != '\n'){
+			//一行没解析完
+
+			//解码第一个编码
+			value = DecodeTable[strData[i++]]<<18;
+
+			//解码第二个编码
+			value += DecodeTable[strData[i++]] << 12;
+			strDecode += ((value >> 16) & 0xFF);
+
+			if (strData[i] != '='){
+				//解码第三个编码
+				value += DecodeTable[strData[i++]] << 6;
+				strDecode += ((value >> 8) & 0xFF);
+
+				if (strData[i] != '='){
+					//解码第四个编码
+					value += DecodeTable[strData[i++]];
+					strDecode += (value & 0xFF);
+				}
+			}
+			else{
+				break;
+			}
+
+
+		}
+		else{
+			//解码到达末尾，跳过"\r\n"
+			i += 2;
+		}
+	}
 
 
 	return strDecode;
--- C++项目/base64/base64/Base64Tool.cpp
+++ C++项目/base64/base64/Base64Tool.cpp
@@ -3,14 +3,32 @@
 #include <string>
 using namespace std;
 
-int main(){
+//±àÂë
+void Test_Encode(){
 	string enData;
-	while (getline(cin, enData)){
-		Base64 str;
-		string enstr=str.Encode(enData);
-		cout << enstr << endl;
 
-	}
+	cout << "enData:";
+	getline(cin, enData);
+	Base64 str;
+	string enstr = str.Encode(enData);
+	cout << enstr << endl;
+}
+
+//½âÂë
+void Test_Decode(){
+	string deData;
+
+	cout << "deData:";
+	getline(cin, deData);
+	Base64 str;
+	string destr = str.Decode(deData);
+	cout << destr << endl;
+}
+
+int main(){
+	Test_Encode();
+	Test_Decode();
+
 
 	return 0;
 }
